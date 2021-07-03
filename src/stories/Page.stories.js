7f56a82e00c4e6bb5f1322552c2d84095a22022b
abhishek.singh <abhishek.singh@arista.com>
2021-07-03 10:48:54 +0530

    Make storybook run

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Page } from './Page';
-import * as HeaderStories from './Header.stories';
+// import * as HeaderStories from './Header.stories';
 
 export default {
   title: 'Example/Page',
@@ -12,10 +12,10 @@ const Template = (args) => <Page {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  ...HeaderStories.LoggedIn.args,
+  // ...HeaderStories.LoggedIn.args,
 };
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
+  // ...HeaderStories.LoggedOut.args,
 };
