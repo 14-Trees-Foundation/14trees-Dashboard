b9e3e56f2d5dfaf22534b939b59db55473cbed96
unknown <ritwik18920@gmail.com>
2021-08-13 11:44:13 +0530

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Button from "./stories/buttons/Button";
-
-function hello() {
-  alert("clicked");
-}
+// import Button from "./stories/buttons/Button";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./stories/navbar/navbar";
+import Home from "./stories/navbar/Pages/Home";
+import About from "./stories/navbar/Pages/About";
+// function hello() {
+//   alert("clicked");
+// }
 
 function App() {
   return (
     <div className="App">
-      <Button label="Search" size="large" onClick={hello}></Button>
+      {/* <Button label="Search" size="large" onClick={hello}></Button> */}
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
     </div>
   );
 }
