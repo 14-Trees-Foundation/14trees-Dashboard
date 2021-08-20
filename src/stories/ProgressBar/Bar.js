8a09a2895a576840d9f2c8627e7ddda5bc6e5853
ab-si <abhisingh7294@gmail.com>
2021-08-20 10:53:24 +0530

    Fix scroll issue

--- src/stories/ProgressBar/Bar.js
+++ src/stories/ProgressBar/Bar.js
@@ -7,8 +7,8 @@ const Setvalue = (20560 / 100000) * 100;
 export const Bar = ({ value, max, ...props }) => {
   return (
     <div className="bar">
-      <h1 className="heading">Trees planted till date,</h1>
-      <p className="count">20,560/100,000</p>
+      <h1 className="heading">Trees planted till date</h1>
+      <div className="count">20,560/100,000</div>
       <progress value={Setvalue} max={max} className="progress" {...props} />
     </div>
   );
@@ -21,6 +21,7 @@ Bar.propTypes = {
 
 Bar.defaultProps = {
   max: "100",
+  value: "80"
 };
 
 export default Bar;
