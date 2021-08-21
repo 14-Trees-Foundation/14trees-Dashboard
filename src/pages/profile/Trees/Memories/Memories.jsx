80122184ad2a8c269ff77c632b28baee0c40d293
ab-si <abhisingh7294@gmail.com>
2021-08-21 21:20:11 +0530

    Fix layout for memories

--- src/pages/profile/Trees/Memories/Memories.jsx
+++ src/pages/profile/Trees/Memories/Memories.jsx
@@ -1,7 +1,21 @@
-export const Memories = () => {
-    return (
-        <div>
-            Memories
-        </div>
-    )
+import './memories.scss'
+
+export const Memories = (props) => {
+    let isOverlay = props.overlay;
+    if (isOverlay === "false") {
+        return (
+            <div>
+                <img alt="Card" src="https://picsum.photos/536/354" className="memory"/>
+            </div>
+        )
+    } else {
+        return (
+            <div className="memory">
+                <img alt="Card" src="https://picsum.photos/536/354" className="memory"/>
+                <div className="overlay">
+                    <h3 className="text">See More</h3>
+                </div>
+            </div>
+        )
+    }
 }
--- src/pages/profile/Trees/Trees.jsx
+++ src/pages/profile/Trees/Trees.jsx
@@ -7,15 +7,41 @@ import 'primeflex/primeflex.css';
 export const Trees = () => {
     const { saplingId } = useParams();
     return (
-        <div className="memories">
-            <h2>Trees Planted</h2>
-            <div className="p-grid">
-                <div className="p-lg-3 p-md-3" style={{"padding":"0.2rem"}}>
-                    <TreesPlanted/>
+        <div className="trees">
+            {/* <h2>Trees Planted</h2> */}
+            <div className="p-grid nested-grid">
+                <div className="p-col-12 p-lg-6 p-md-6" style={{"padding":0}}>
+                    <div className="p-grid" style={{"margin":0}}>
+                        <div className="p-col-12" style={{"padding":0}}>
+                            <h2 style={{"margin":"0 0 5px 0"}}>Trees Planted</h2>
+                        </div>
+                        <div className="p-col-6 p-lg-6 p-md-6" style={{"padding":0}}>
+                            <TreesPlanted/>
+                        </div>
+                        <div className="p-col-6 p-lg-6 p-md-6" style={{"padding":0}}>
+                            <TreesPlanted/>
+                        </div>
+                    </div>
                 </div>
-                <div className="p-lg-3 p-md-3" style={{"padding":"0.2rem"}}>
-                    <TreesPlanted/>
-                </div>    
+                <div className="p-col-12 p-lg-6 p-md-6" style={{"padding":0}}>
+                    <div className="p-grid" style={{"margin":0}}>
+                        <div className="p-col-12" style={{"padding":0}}>
+                            <h2 style={{"margin":"0 0 5px 0"}}>Memories</h2>
+                        </div>
+                            <div className="p-col-7 p-lg-7 p-md-7" style={{"padding":"0 0.3rem 0 0"}}>
+                                <Memories overlay="false"/>
+                            </div>
+                            <div className="p-col-5 p-lg-5 p-md-5" style={{"padding":"0 0.3rem 0 0"}}>
+                                <Memories overlay="false"/>
+                            </div>
+                            <div className="p-col-7 p-lg-7 p-md-7" style={{"padding":"0 0.3rem 0 0"}}>
+                                <Memories overlay="false"/>
+                            </div>
+                            <div className="p-col-5 p-lg-5 p-md-5" style={{"padding":"0 0.3rem 0 0"}}>
+                                <Memories overlay="true"/>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
