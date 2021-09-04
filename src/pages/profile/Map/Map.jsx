a7ddefd6a298d60054397ae11e341bffc0eb4740
abhishek.singh <abhishek.singh@arista.com>
2021-09-04 21:38:51 +0530

    Remove shadows

--- src/pages/profile/Map/Map.jsx
+++ src/pages/profile/Map/Map.jsx
@@ -8,7 +8,7 @@ const containerStyle = {
     width: '100%',
     height: '101%',
     borderRadius: '6px',
-    boxShadow: '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)'
+    // boxShadow: '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)'
 };
 
 const paths = [
