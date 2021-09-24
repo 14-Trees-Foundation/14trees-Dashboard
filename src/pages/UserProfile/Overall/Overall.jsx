97dea1a069b421d5ed89f0d55f58c44d4e16c0d8
ab-si <abhisingh7294@gmail.com>
2021-09-24 22:44:54 +0530

    Remove bar from overall

--- src/pages/UserProfile/Overall/Overall.jsx
+++ src/pages/UserProfile/Overall/Overall.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { Impact } from "../../../stories/Impact/Impact";
-import { Bar } from "../../../stories/ProgressBar/Bar";
 import { Popup } from "../../../stories/Popup/Popup";
 import { PopupItem } from "../../../stories/PopupItem/PopupItem";
 import './overall.scss';
 import 'primeflex/primeflex.css';
 
-export const Overall = ({trees, ponds}) => {
+export const Overall = ({ trees, ponds }) => {
 
     const [popup, setPopup] = useState(false);
 
@@ -25,24 +24,21 @@ export const Overall = ({trees, ponds}) => {
     return (
         <div className="overall">
             <Popup display={popup} toggle={togglePopup}>
-                <PopupItem/>
+                <PopupItem />
             </Popup>
             <h2>Overall Impact</h2>
             <div className="p-grid">
                 <div
                     className="p-col-12 p-md-3 p-sm-6"
-                    style={{"cursor":"pointer"}}
+                    style={{ "cursor": "pointer" }}
                     onClick={() => getPonds()}>
-                    <Impact count={trees.count} text={"Trees Planted by visitors till date"}/>
+                    <Impact count={trees.count} text={"Trees Planted by visitors till date"} />
                 </div>
                 <div
                     className="p-col-12 p-md-3 p-sm-6"
-                    style={{"cursor":"pointer"}}
+                    style={{ "cursor": "pointer" }}
                     onClick={() => getTrees()}>
-                    <Impact count={"100+"} text={"People employed from local community."}/>
-                </div>
-                <div className="p-col-12 p-md-6 p-sm-12">
-                    <Bar/>
+                    <Impact count={"100+"} text={"People employed from local community."} />
                 </div>
             </div>
         </div>
