ffabecef35c1cc07583b2635a7b24395bdaed305
ab-si <abhisingh7294@gmail.com>
2022-01-07 16:22:55 +0530

    Add spinner compoonent

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,42 @@
+import { makeStyles } from '@mui/styles';
+import gif from '../assets/loader_light.gif';
+
+export const Spinner = ({text}) => {
+    const classes = usestyle();
+
+    if(text === "" || text === undefined) {
+        text = "This forest is dense, taking some time to reach your destination!";
+    }
+    return (
+            <div className={classes.spinner}>
+                <img className={classes.img} src={gif} alt="Loader"/>
+                <p className={classes.text}>{text}</p>
+            </div>
+    );
+};
+
+const usestyle = makeStyles((theme) => ({
+    spinner: {
+        background: '#1f3625',
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        left: '0',
+        right: '0'
+    },
+    img: {
+        display: 'block',
+        marginTop: '15%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '200px',
+        height: '200px',
+        marginBottom: '2%'
+    },
+    text: {
+        fontSize: '30px',
+        color: '#ffffff',
+        fontWeight: '500',
+        textAlign: 'center'
+    }
+}))
