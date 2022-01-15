82d062b8305152ca1cfc2df367f585d305edb16e
ab-si <abhisingh7294@gmail.com>
2022-01-15 20:23:42 +0530

    Memory changes

--- src/pages/UserProfile/Memories/Memories.jsx
+++ src/pages/UserProfile/Memories/Memories.jsx
@@ -22,14 +22,14 @@ export const Memories = () => {
         images.push.apply(images, tree['memories']);
     }
 
-    if (images.length === 1 && images[0] === '') {
-        images = [7, 6, 1, 3, 5, 4, 8, 9, 1, 11, 12, 13, 14, 15, 23, 16, 17, 18, 19, 20, 21, 22].map((number) => {
-            return `https://14treesplants.s3.ap-south-1.amazonaws.com/memories/memory${number}.jpg`
-        });
+    let allImages = [];
+    allImages = [7, 6, 1, 3, 5, 4, 8, 9, 1, 11, 12, 13, 14, 15, 23, 16, 17, 18, 19, 20, 21, 22].map((number) => {
+        return `https://14treesplants.s3.ap-south-1.amazonaws.com/memories/memory${number}.jpg`
+    });
 
-    }
     images = [...new Set(images)]
     images = images.filter(function (e) { return e });
+    images = [...images, ...allImages]
 
     const next = () => {
         if (index < images.length - 1) {
