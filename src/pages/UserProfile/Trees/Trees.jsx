ba08568aa1617aae564a6440377fa8c191aa924a
Abhishek Singh <phoenix@Abhisheks-MacBook-Pro.local>
2022-03-13 13:45:31 +0530

    Closes #14; Fix tree image

--- src/pages/UserProfile/Trees/Trees.jsx
+++ src/pages/UserProfile/Trees/Trees.jsx
@@ -17,7 +17,6 @@ export const Trees = () => {
         images.push.apply(images, tree['memories']);
     }
 
-    console.log(userinfo)
     return (
         <div className={classes.main}>
             <div className={classes.card}>
@@ -42,7 +41,7 @@ export const Trees = () => {
                                                 <TreesPlanted
                                                     id={tree.tree.sapling_id}
                                                     name={tree.tree.tree_id.name}
-                                                    img={tree.tree.image[0] !== undefined ? tree.tree.image[0] : tree.tree.tree_id.image[0]}
+                                                    img={tree.tree.image[0] === '' ? tree.tree.tree_id.image[0] : tree.tree.image[0]}
                                                     date={date}
                                                     selected={true} />
                                                 :
