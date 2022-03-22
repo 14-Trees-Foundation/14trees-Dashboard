5bf4a44bf4699202fc5c632378e5213913d9a518
Abhishek Singh <abhisingh7294@gmail.com>
2022-03-22 13:49:53 +0530

    Fix tree image and add onclick tree popup

--- src/pages/UserProfile/Trees/Trees.jsx
+++ src/pages/UserProfile/Trees/Trees.jsx
@@ -1,99 +1,118 @@
-import { createStyles, makeStyles } from '@mui/styles';
-import { useRecoilValue, useRecoilState } from 'recoil';
+import { createStyles, makeStyles } from "@mui/styles";
+import { useRecoilValue, useRecoilState } from "recoil";
 
-import { TreesPlanted } from '../../../stories/TreesPlanted/TreesPlanted';
-import { usersData, currSelTree } from '../../../store/atoms';
+import { TreesPlanted } from "../../../stories/TreesPlanted/TreesPlanted";
+import { usersData, currSelTree } from "../../../store/atoms";
 
 export const Trees = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const userinfo = useRecoilValue(usersData);
+  const [currTree, setCurrTree] = useRecoilState(currSelTree);
+  // const setIndex = useSetRecoilState(navIndex);
+  let numTrees = userinfo.trees.length;
+  let images = [];
+  for (const tree of userinfo.trees) {
+    images.push.apply(images, tree["memories"]);
+  }
 
-    const userinfo = useRecoilValue(usersData);
-    const [currTree, setCurrTree] = useRecoilState(currSelTree);
-    // const setIndex = useSetRecoilState(navIndex);
-    let numTrees = userinfo.trees.length
-    let images = [];
-    for (const tree of userinfo.trees) {
-        images.push.apply(images, tree['memories']);
-    }
-
-    return (
-        <div className={classes.main}>
-            <div className={classes.card}>
-                <div style={{ display: 'flex', lineHeight: '30px', padding: '10px 0 0 10px' }}>
-                    <div style={{ padding: "1%", fontSize: '14px' }}>Trees Planted ({numTrees})</div>
-                    {/* {
+  return (
+    <div className={classes.main}>
+      <div className={classes.card}>
+        <div
+          style={{
+            display: "flex",
+            lineHeight: "30px",
+            padding: "10px 0 0 10px",
+          }}
+        >
+          <div style={{ padding: "1%", fontSize: "14px" }}>
+            Trees Planted ({numTrees})
+          </div>
+          {/* {
                         numTrees > 2 &&
                         <div style={{ marginLeft: 'auto', marginRight: '5%' }}>
                             <Chip label={"See All >"} mode={'primary'} size={'small'} handleClick={handleSeeAllClick}/>
                         </div>
                     } */}
-                </div>
-                <div className={classes.trees}>
-                    <div className={classes.scroll}>
-                        {
-                            userinfo.trees.map((tree, idx) => {
-                                const date = tree.tree.date_added.slice(0, 10)
-                                return (
-                                    <div key={idx} onClick={() => setCurrTree(idx)} style={{ cursor: 'pointer' }}>
-                                        {
-                                            currTree === idx ?
-                                                <TreesPlanted
-                                                    id={tree.tree.sapling_id}
-                                                    name={tree.tree.tree_id.name}
-                                                    img={tree.tree.image[0] === '' ? tree.tree.image[0]: tree.tree.tree_id.image[0]}
-                                                    date={date}
-                                                    selected={true} />
-                                                :
-                                                <TreesPlanted
-                                                    id={tree.tree.sapling_id}
-                                                    name={tree.tree.tree_id.name}
-                                                    img={tree.tree.tree_id.image[0]}
-                                                    date={date} />
-                                        }
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
         </div>
-    )
-}
+        <div className={classes.trees}>
+          <div className={classes.scroll}>
+            {userinfo.trees.map((tree, idx) => {
+              const date = tree.tree.date_added.slice(0, 10);
+              return (
+                <div
+                  key={idx}
+                  onClick={() => setCurrTree(idx)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {currTree === idx ? (
+                    <TreesPlanted
+                      id={tree.tree.sapling_id}
+                      name={tree.tree.tree_id.name}
+                      img={
+                        tree.tree.image.length === 0 ||
+                        tree.tree.image[0] === ""
+                          ? tree.tree.tree_id.image[0]
+                          : tree.tree.image[0]
+                      }
+                      date={date}
+                      selected={true}
+                    />
+                  ) : (
+                    <TreesPlanted
+                      id={tree.tree.sapling_id}
+                      name={tree.tree.tree_id.name}
+                      img={
+                        tree.tree.image.length === 0 ||
+                        tree.tree.image[0] === ""
+                          ? tree.tree.tree_id.image[0]
+                          : tree.tree.image[0]
+                      }
+                      date={date}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const useStyles = makeStyles((theme) =>
-    createStyles({
-        main: {
-            width: '100%',
-            height: '100%',
-        },
-        card: {
-            backgroundColor: '#ffffff',
-            borderRadius: '12px',
-            height: '100%',
-            boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.15)',
-        },
-        trees: {
-            width: 'calc(100% - 30px)',
-            marginLeft: '15px',
-            paddingTop: '5px',
-            margin: '10px',
-            height: '90%',
-        },
-        scroll: {
-            maxHeight: 'calc(100% - 50px)',
-            overflowX: 'hidden',
-            overflowY: 'auto',
-            '&::-webkit-scrollbar': {
-                width: '0.2em',
-
-            },
-            '&::-webkit-scrollbar-thumb': {
-                backgroundColor: '#1F3625',
-                borderRadius: '0.3em',
-                height: '10px'
-            },
-        }
-    })
-)
+  createStyles({
+    main: {
+      width: "100%",
+      height: "100%",
+    },
+    card: {
+      backgroundColor: "#ffffff",
+      borderRadius: "12px",
+      height: "100%",
+      boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.15)",
+    },
+    trees: {
+      width: "calc(100% - 30px)",
+      marginLeft: "15px",
+      paddingTop: "5px",
+      margin: "10px",
+      height: "90%",
+    },
+    scroll: {
+      maxHeight: "calc(100% - 50px)",
+      overflowX: "hidden",
+      overflowY: "auto",
+      "&::-webkit-scrollbar": {
+        width: "0.2em",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "#1F3625",
+        borderRadius: "0.3em",
+        height: "10px",
+      },
+    },
+  })
+);
--- src/stories/TreesPlanted/TreesPlanted.jsx
+++ src/stories/TreesPlanted/TreesPlanted.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { createStyles, makeStyles } from '@mui/styles';
+import { createStyles, makeStyles } from "@mui/styles";
+import { Dialog } from "@mui/material";
 
 export const TreesPlanted = (props) => {
+  const [img, setImg] = useState(null);
+  const [open, setOpenPopup] = useState(false);
+  const onTogglePop = () => {
+    setOpenPopup(!open);
+  };
+  const handleImgClick = (val) => {
+    setOpenPopup(!open);
+    setImg(val);
+  };
 
   const classes = useStyles(props);
-
   return (
     <div className={classes.main}>
-      <img src={props.img} alt={"Tree"} className={classes.img} />
-      <div style={{ marginLeft: '15px', marginTop: '10px' }}>
-        <div style={{ fontSize: '18px', fontWeight: '700' }}>{props.name}</div>
+      <div>
+        <Dialog onClose={() => onTogglePop()} open={open}>
+          <img style={{ width: "100%", height: "auto" }} src={img} alt={"A"} />
+        </Dialog>
+      </div>
+      <img
+        src={props.img}
+        alt={"Tree"}
+        className={classes.img}
+        onClick={() => handleImgClick(props.img)}
+      />
+      <div style={{ marginLeft: "15px", marginTop: "10px" }}>
+        <div style={{ fontSize: "18px", fontWeight: "700" }}>{props.name}</div>
         <div className={classes.data}>Tree ID : {props.id}</div>
         <div className={classes.data}>Date : {props.date}</div>
         {/* <div className={classes.data}>Event : {props.event}</div> */}
@@ -31,48 +50,48 @@ TreesPlanted.propTypes = {
 TreesPlanted.defaultProps = {
   mode: "secondary",
   img: "",
-  event: 'Independent Event',
-  selected: false
+  event: "Independent Event",
+  selected: false,
 };
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     main: {
-      width: '91%',
-      maxHeight: '130px',
-      padding: '15px',
-      margin: '0 0px 10px 4px',
-      borderRadius: '12px',
-      display: 'flex',
-      backgroundColor: props => props.selected ? '#1f3625' : '#ffffff',
-      color: props => props.selected ? '#ffffff' : '#000000',
-      boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.15)',
-      [theme.breakpoints.down('1500')]: {
-        width: '85%',
-      }
+      width: "91%",
+      maxHeight: "130px",
+      padding: "15px",
+      margin: "0 0px 10px 4px",
+      borderRadius: "12px",
+      display: "flex",
+      backgroundColor: (props) => (props.selected ? "#1f3625" : "#ffffff"),
+      color: (props) => (props.selected ? "#ffffff" : "#000000"),
+      boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.15)",
+      [theme.breakpoints.down("1500")]: {
+        width: "85%",
+      },
     },
     selected: {
-      color: '#ffffff',
-      backgroundColor: '#1F3625'
+      color: "#ffffff",
+      backgroundColor: "#1F3625",
     },
     img: {
-      width: '160px',
-      height: '120px',
-      objectFit: 'cover',
-      borderRadius: '12px'
+      width: "160px",
+      height: "120px",
+      objectFit: "cover",
+      borderRadius: "12px",
     },
     data: {
-      fontSize: '15px',
-      fontWeight: '500',
-      marginTop: '3px',
-      [theme.breakpoints.down('1500')]: {
-        fontSize: '13px',
-        marginTop: '5px',
+      fontSize: "15px",
+      fontWeight: "500",
+      marginTop: "3px",
+      [theme.breakpoints.down("1500")]: {
+        fontSize: "13px",
+        marginTop: "5px",
       },
-      [theme.breakpoints.down('1500')]: {
-        fontSize: '11px',
-      }
-    }
+      [theme.breakpoints.down("1500")]: {
+        fontSize: "11px",
+      },
+    },
   })
 );
 
