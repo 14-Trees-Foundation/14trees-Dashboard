d9f3d9403edf6effdd49f105d510e568bf04e153
Abhishek Singh <abhisingh7294@gmail.com>
2022-04-01 22:50:40 +0530

    Add memorial dashboard filters

--- src/pages/UserProfile/Profile.jsx
+++ src/pages/UserProfile/Profile.jsx
@@ -1,172 +1,189 @@
-import { createStyles, makeStyles } from '@mui/styles';
-import Divider from '@mui/material/Divider';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import { createStyles, makeStyles } from "@mui/styles";
+import Divider from "@mui/material/Divider";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 // import { useState } from "react";
 import { UserInfo } from "./UserInfo/UserInfo";
 import { Trees } from "./Trees/Trees";
 import { Map } from "./Map/Map";
 
-import { useRecoilValue } from 'recoil';
-import { usersData } from '../../store/atoms';
+import { useRecoilValue } from "recoil";
+import { usersData } from "../../store/atoms";
 
-import logo from "../../assets/icon_round.png"
-import { Button } from '@mui/material';
+import logo from "../../assets/icon_round.png";
+import { Button } from "@mui/material";
 
 export const Profile = () => {
-    const matches = useMediaQuery('(max-width:481px)');
-    const classes = useStyles();
+  const matches = useMediaQuery("(max-width:481px)");
+  const classes = useStyles();
 
-    const userinfo = useRecoilValue(usersData);
-    // const [activeStep, setActiveStep] = useState(0);
+  const userinfo = useRecoilValue(usersData);
+  // const [activeStep, setActiveStep] = useState(0);
 
-    // const handleInfoChange = (i) => {
-    //     setActiveStep(i)
-    // }
+  // const handleInfoChange = (i) => {
+  //     setActiveStep(i)
+  // }
 
-    const username = userinfo.user.user.name.split(" ")[0]
+  const username = userinfo.user.user.name.split(" ")[0];
+  console.log(userinfo);
 
-    const header = () => {
-        if(!matches) {
-            return(
-                <div className={classes.header}>
-                    <img src={logo} alt={logo} className={classes.img} />
-                    <div className={classes.username}>
-                        {username}'s Dashboard
-                    </div>
-                    <div style={{justifyContent:'flex-end'}}>
-                        <Button color='primary' variant='contained' onClick={() => {window.open("https://14trees.org/projects/reforestation-vetale/")}}>
-                            Gift a Tree
-                        </Button>
-                    </div>
-                </div>
-            )
-        }
-    }
-
-    return (
-        <div className={matches?classes.mbmain :classes.main}>
-            {
-               header()
-            }
-            <Divider style={{ marginLeft: '4%', marginRight: '4%' }} />
-            <div style={{ padding: '4%' }}>
-                <div className={classes.user}>
-                    <UserInfo />
-                </div>
-                <div className={classes.treemap}>
-                    <div style={{ display: matches ? 'block' : 'flex' }}>
-                        <div className={classes.tree}><Trees /></div>
-                        <div className={classes.map}><Map /></div>
-                    </div>
-                </div>
+  const header = () => {
+    if (!matches) {
+      return (
+        <div className={classes.header}>
+          <img src={logo} alt={logo} className={classes.img} />
+          <div className={classes.username}>
+            {userinfo.user.event && userinfo.user.event.type === "2"
+              ? "Memorial Dashboard"
+              : `${username}'s Dashboard`}
+          </div>
+          {userinfo.user.event && userinfo.user.event.type === "2" ? (
+            ""
+          ) : (
+            <div style={{ justifyContent: "flex-end" }}>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={() => {
+                  window.open(
+                    "https://14trees.org/projects/reforestation-vetale/"
+                  );
+                }}
+              >
+                Gift a Tree
+              </Button>
             </div>
+          )}
         </div>
-    )
-}
+      );
+    }
+  };
+
+  return (
+    <div className={matches ? classes.mbmain : classes.main}>
+      {header()}
+      <Divider style={{ marginLeft: "4%", marginRight: "4%" }} />
+      <div style={{ padding: "4%" }}>
+        <div className={classes.user}>
+          <UserInfo />
+        </div>
+        <div className={classes.treemap}>
+          <div style={{ display: matches ? "block" : "flex" }}>
+            <div className={classes.tree}>
+              <Trees />
+            </div>
+            <div className={classes.map}>
+              <Map />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const useStyles = makeStyles((theme) =>
-    createStyles({
-        mbmain:{
-            height: '100%',
-            marginTop: '50px'
-        },
-        main: {
-            height: '100%',
-        },
-        img:{
-            width: '50px',
-            height: '50px',
-            marginLeft:'0',
-            [theme.breakpoints.down('480')]: {
-                width: '35px',
-                height: '35px',
-            }
-        },
-        header:{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            height: '5vh',
-            padding: '3.5vh',
-            [theme.breakpoints.down('1500')]: {
-                height: '4vh',
-                padding: '3vh',
-            },
-            [theme.breakpoints.down('480')]: {
-                height: '3vh',
-                padding: '3vh',
-            }
-        },
-        username: {
-            lineHeight: '50px',
-            fontSize: '34px',
-            color: '#1F3625',
-            fontWeight: '500',
-            marginLeft: '20px',
-            marginRight: 'auto',
-            [theme.breakpoints.down('1500')]: {
-                lineHeight: '40px',
-                fontSize: '28px',
-            },
-            [theme.breakpoints.down('480')]: {
-                lineHeight: '40px',
-                fontSize: '20px',
-            }
-        },
-        user: {
-            display: 'flex',
-            minHeight: '22vh',
-            [theme.breakpoints.down('1500')]: {
-                minHeight: '32vh',
-            },
-            [theme.breakpoints.down('481')]: {
-                minHeight: '100%',
-            },
-        },
-        treemap: {
-            fontSize: '30px',
-            height: '50vh',
-            marginTop: '20px',
-            [theme.breakpoints.down('1500')]: {
-                height: '52vh',
-                marginTop: '5px',
-            },
-        },
-        tree: {
-            width: '40%',
-            height: '50vh',
-            zIndex: '1',
-            [theme.breakpoints.down('1500')]: {
-                height: '52vh',
-            },
-            [theme.breakpoints.down('1025')]: {
-                width: '50%',
-            },
-            [theme.breakpoints.down('480')]: {
-                width: '100%',
-                height: '60%',
-                marginRight: '0',
-                marginTop: '10px',
-                marginBottom: '-30px',
-                position: 'relative'
-            },
-        },
-        map: {
-            width: '60%',
-            height: '50vh',
-            [theme.breakpoints.down('1500')]: {
-                height: '52vh',
-            },
-            [theme.breakpoints.down('1025')]: {
-                width: '55%',
-            },
-            [theme.breakpoints.down('480')]: {
-                width: '100%',
-                height: '60vh',
-                marginRight: '0px',
-                marginLeft: '0px',
-                paddingBottom: '20px',
-            },
-        }
-    })
+  createStyles({
+    mbmain: {
+      height: "100%",
+      marginTop: "50px",
+    },
+    main: {
+      height: "100%",
+    },
+    img: {
+      width: "50px",
+      height: "50px",
+      marginLeft: "0",
+      [theme.breakpoints.down("480")]: {
+        width: "35px",
+        height: "35px",
+      },
+    },
+    header: {
+      display: "flex",
+      justifyContent: "flex-end",
+      height: "5vh",
+      padding: "3.5vh",
+      [theme.breakpoints.down("1500")]: {
+        height: "4vh",
+        padding: "3vh",
+      },
+      [theme.breakpoints.down("480")]: {
+        height: "3vh",
+        padding: "3vh",
+      },
+    },
+    username: {
+      lineHeight: "50px",
+      fontSize: "34px",
+      color: "#1F3625",
+      fontWeight: "500",
+      marginLeft: "20px",
+      marginRight: "auto",
+      [theme.breakpoints.down("1500")]: {
+        lineHeight: "40px",
+        fontSize: "28px",
+      },
+      [theme.breakpoints.down("480")]: {
+        lineHeight: "40px",
+        fontSize: "20px",
+      },
+    },
+    user: {
+      display: "flex",
+      minHeight: "22vh",
+      [theme.breakpoints.down("1500")]: {
+        minHeight: "32vh",
+      },
+      [theme.breakpoints.down("481")]: {
+        minHeight: "100%",
+      },
+    },
+    treemap: {
+      fontSize: "30px",
+      height: "50vh",
+      marginTop: "20px",
+      [theme.breakpoints.down("1500")]: {
+        height: "52vh",
+        marginTop: "5px",
+      },
+    },
+    tree: {
+      width: "40%",
+      height: "50vh",
+      zIndex: "1",
+      [theme.breakpoints.down("1500")]: {
+        height: "52vh",
+      },
+      [theme.breakpoints.down("1025")]: {
+        width: "50%",
+      },
+      [theme.breakpoints.down("480")]: {
+        width: "100%",
+        height: "60%",
+        marginRight: "0",
+        marginTop: "10px",
+        marginBottom: "-30px",
+        position: "relative",
+      },
+    },
+    map: {
+      width: "60%",
+      height: "50vh",
+      [theme.breakpoints.down("1500")]: {
+        height: "52vh",
+      },
+      [theme.breakpoints.down("1025")]: {
+        width: "55%",
+      },
+      [theme.breakpoints.down("480")]: {
+        width: "100%",
+        height: "60vh",
+        marginRight: "0px",
+        marginLeft: "0px",
+        paddingBottom: "20px",
+      },
+    },
+  })
 );
--- src/pages/UserProfile/UserInfo/UserInfo.jsx
+++ src/pages/UserProfile/UserInfo/UserInfo.jsx
@@ -1,216 +1,240 @@
-import { Fragment } from 'react';
-import { Dialog, Grid } from '@mui/material';
-import { createStyles, makeStyles } from '@mui/styles';
-import { useRecoilValue, useRecoilState } from 'recoil';
+import { Fragment } from "react";
+import { Dialog, Grid } from "@mui/material";
+import { createStyles, makeStyles } from "@mui/styles";
+import { useRecoilValue, useRecoilState } from "recoil";
 // import { useSetRecoilState } from 'recoil';
 
 import { Memories } from "../Memories/Memories";
 import { InfoChip } from "../../../stories/InfoChip/InfoChip";
-import { usersData, openProfilePopup } from '../../../store/atoms';
+import { usersData, openProfilePopup } from "../../../store/atoms";
 // import { navIndex } from '../../../store/atoms';
 
 export const UserInfo = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const userinfo = useRecoilValue(usersData);
-    // const setIndex = useSetRecoilState(navIndex);
-    const [open, setOpenPopup] = useRecoilState(openProfilePopup);
-    const handleTreeClick = () => {
-        // setIndex(2);
-    }
-    const onTogglePop = () => {
-        setOpenPopup(!open)
-    }
+  const userinfo = useRecoilValue(usersData);
+  // const setIndex = useSetRecoilState(navIndex);
+  const [open, setOpenPopup] = useRecoilState(openProfilePopup);
+  const handleTreeClick = () => {
+    // setIndex(2);
+  };
+  const onTogglePop = () => {
+    setOpenPopup(!open);
+  };
 
-    const handleOpenPopup = () => {
-        setOpenPopup(true)
-    }
+  const handleOpenPopup = () => {
+    setOpenPopup(true);
+  };
 
-    const treeDoneWidth = (userinfo.trees.length / 14) * 100;
+  const treeDoneWidth = (userinfo.trees.length / 14) * 100;
 
-    if (open) {
-        return (
-            <div>
-                <Dialog onClose={onTogglePop} open={open}>
-                    <img className={classes.imageWindow} src={userinfo.user.profile_image[0]} alt={"A"} />
-                </Dialog>
-            </div>
-        )
-    } else {
-        return (
-            <div style={{ width: '100%', height: '100%' }}>
-                <Grid container>
-                    <Grid item xs={6} md={3}>
-                        <img
-                            onClick={() => handleOpenPopup()}
-                            className={classes.userimg}
-                            alt="Card"
-                            src={userinfo.user.profile_image[0] === "" ? userinfo.trees[0].tree.tree_id.image[0] : userinfo.user.profile_image[0]} />
-                    </Grid>
-                    <Grid item xs={6} md={3} className={classes.infobox}>
-                        <div className={classes.info}>
-                            <div className={classes.label}>Name</div>
-                            <div className={classes.data}>{userinfo.user.user.name}</div>
-                            {
-                                (userinfo.user.donated_by === undefined || userinfo.user.donated_by === null)
-                                    ?
-                                    (
-                                        <Fragment>
-                                            <div className={classes.label}>Organization</div>
-                                            <div className={classes.data}>{userinfo.user.orgid.name}</div>
-                                            <div className={classes.growth}>
-                                                <div style={{ marginTop: '20px' }}>
-                                                    <div style={{ display: 'flex' }}>
-                                                        <InfoChip count={userinfo.trees.length} label="Trees Planted" onClick={handleTreeClick} />
-                                                        {/* TODO: Events attended configuration on backend */}
-                                                        {/* <InfoChip count={userinfo.trees.length} label="Events attended" /> */}
-                                                    </div>
-                                                    <div className={classes.overall}>
-                                                        <div className={classes.done} style={{ "width": `${treeDoneWidth}%` }}>
-                                                        </div>
-                                                        <div className={classes.count}>
-                                                            {14 - userinfo.trees.length}
-                                                            <div className={classes.countdesc}>
-                                                                Trees away from neutralising your carbon footprint
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Fragment>
-                                    )
-                                    :
-                                    <>
-                                        <div className={classes.label}>Planted By</div>
-                                        <div className={classes.data}>{userinfo.user.donated_by.name}</div>
-                                        <div className={classes.label}>Tree Name</div>
-                                        <div className={classes.data}>{userinfo.trees[0].tree.tree_id.name}</div>
-                                        <div className={classes.label}>Location</div>
-                                        <div className={classes.data}>{userinfo.trees[0].tree.plot_id.name}</div>
-                                    </>
-                            }
+  if (open) {
+    return (
+      <div>
+        <Dialog onClose={onTogglePop} open={open}>
+          <img
+            className={classes.imageWindow}
+            src={userinfo.user.profile_image[0]}
+            alt={"A"}
+          />
+        </Dialog>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        <Grid container>
+          <Grid item xs={6} md={3}>
+            <img
+              onClick={() => handleOpenPopup()}
+              className={classes.userimg}
+              alt="Card"
+              src={
+                userinfo.user.profile_image[0] === ""
+                  ? userinfo.trees[0].tree.tree_id.image[0]
+                  : userinfo.user.profile_image[0]
+              }
+            />
+          </Grid>
+          <Grid item xs={6} md={3} className={classes.infobox}>
+            <div className={classes.info}>
+              <div className={classes.label}>Name</div>
+              <div className={classes.data}>{userinfo.user.user.name}</div>
+              {userinfo.user.donated_by === undefined ||
+              userinfo.user.donated_by === null ? (
+                <Fragment>
+                  <div className={classes.label}>Organization</div>
+                  <div className={classes.data}>{userinfo.user.orgid.name}</div>
+                  <div className={classes.growth}>
+                    <div style={{ marginTop: "20px" }}>
+                      <div style={{ display: "flex" }}>
+                        <InfoChip
+                          count={userinfo.trees.length}
+                          label="Trees Planted"
+                          onClick={handleTreeClick}
+                        />
+                        {/* TODO: Events attended configuration on backend */}
+                        {/* <InfoChip count={userinfo.trees.length} label="Events attended" /> */}
+                      </div>
+                      <div className={classes.overall}>
+                        <div
+                          className={classes.done}
+                          style={{ width: `${treeDoneWidth}%` }}
+                        ></div>
+                        <div className={classes.count}>
+                          {14 - userinfo.trees.length}
+                          <div className={classes.countdesc}>
+                            Trees away from neutralising your carbon footprint
+                          </div>
                         </div>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Memories />
-                    </Grid>
-                </Grid>
+                      </div>
+                    </div>
+                  </div>
+                </Fragment>
+              ) : (
+                <>
+                  <div className={classes.label}>Planted By</div>
+                  {userinfo.user.planted_by ? (
+                    <div className={classes.data}>
+                      {userinfo.user.planted_by}
+                    </div>
+                  ) : (
+                    <div className={classes.data}>
+                      {userinfo.user.donated_by.name}
+                    </div>
+                  )}
+                  <div className={classes.label}>Tree Name</div>
+                  <div className={classes.data}>
+                    {userinfo.trees[0].tree.tree_id.name}
+                  </div>
+                  <div className={classes.label}>Location</div>
+                  <div className={classes.data}>
+                    {userinfo.trees[0].tree.plot_id.name}
+                  </div>
+                </>
+              )}
             </div>
-        )
-    }
-}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Memories />
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+};
 
 const useStyles = makeStyles((theme) =>
-    createStyles({
-        grid: {
-            // '& .MuiGrid-root': {
-            //     paddingLeft: '0'
-            // }
-        },
-        userimg: {
-            width: '100%',
-            height: '100%',
-            maxHeight: '260px',
-            borderRadius: '15px',
-            objectFit: 'cover',
-            maxWidth: '250px',
-            cursor: 'pointer',
-            [theme.breakpoints.down('1500')]: {
-                maxHeight: '220px',
-                maxWidth: '210px',
-            }
-        },
-        infobox: {
-            height: '260px',
-            position: 'relative',
-            [theme.breakpoints.down('1500')]: {
-                height: '220px',
-            }
-        },
-        info: {
-            paddingLeft: '20px',
-            paddingTop: '10px',
-            minHeight: '30%',
-            [theme.breakpoints.down('480')]: {
-                paddingLeft: '10px',
-            },
-            [theme.breakpoints.between('481', '900')]: {
-                paddingLeft: '0px',
-                marginLeft: '-10px'
-            }
-        },
-        username: {
-            lineHeight: '50px',
-            fontSize: '28px',
-            color: '#1F3625',
-            paddingLeft: '20px',
-            fontWeight: '500'
-        },
-        label: {
-            fontSize: '13px',
-            fontWeight: '300',
-            marginBottom: '3px',
-            [theme.breakpoints.down('1025')]: {
-                fontSize: '10px',
-            }
-        },
-        data: {
-            fontSize: '17px',
-            fontWeight: '600',
-            marginBottom: '8px',
-            [theme.breakpoints.down('1025')]: {
-                fontSize: '16px',
-            }
-        },
-        growth: {
-            [theme.breakpoints.between('481', '900')]: {
-                paddingLeft: '0px',
-                marginLeft: '-10px'
-            }
-        },
-        overall: {
-            display: 'flex',
-            backgroundColor: '#1F3625',
-            marginTop: '7%',
-            color: '#ffffff',
-            fontWeight: '350',
-            minHeight: '50px',
-            borderRadius: '10px',
-            maxWidth: '180px',
-            [theme.breakpoints.down('1500')]: {
-                minHeight: '40px',
-            },
-        },
-        done: {
-            backgroundColor: '#9BC53D',
-            borderRadius: '10px',
-            alignItems: 'center',
-            fontWeight: '400',
-            fontSize: '25px',
-        },
-        count: {
-            fontWeight: '450',
-            position: 'absolute',
-            marginLeft: '0.25em',
-            marginTop: '0.25em',
-            fontSize: '25px',
-            display: 'flex'
-        },
-        countdesc: {
-            fontSize: '11px',
-            textAlign: 'center',
-            maxWidth: '150px',
-            [theme.breakpoints.down('1025')]: {
-                fontSize: '9px',
-            }
-        },
-        imageWindow: {
-            height: 'auto',
-            borderRadius: '20px',
-            objectFit: 'cover',
-            padding: '2%',
-            width: 'auto',
-            maxWidth: '540px',
-        }
-    })
+  createStyles({
+    grid: {
+      // '& .MuiGrid-root': {
+      //     paddingLeft: '0'
+      // }
+    },
+    userimg: {
+      width: "100%",
+      height: "100%",
+      maxHeight: "260px",
+      borderRadius: "15px",
+      objectFit: "cover",
+      maxWidth: "250px",
+      cursor: "pointer",
+      [theme.breakpoints.down("1500")]: {
+        maxHeight: "220px",
+        maxWidth: "210px",
+      },
+    },
+    infobox: {
+      height: "260px",
+      position: "relative",
+      [theme.breakpoints.down("1500")]: {
+        height: "220px",
+      },
+    },
+    info: {
+      paddingLeft: "20px",
+      paddingTop: "10px",
+      minHeight: "30%",
+      [theme.breakpoints.down("480")]: {
+        paddingLeft: "10px",
+      },
+      [theme.breakpoints.between("481", "900")]: {
+        paddingLeft: "0px",
+        marginLeft: "-10px",
+      },
+    },
+    username: {
+      lineHeight: "50px",
+      fontSize: "28px",
+      color: "#1F3625",
+      paddingLeft: "20px",
+      fontWeight: "500",
+    },
+    label: {
+      fontSize: "13px",
+      fontWeight: "300",
+      marginBottom: "3px",
+      [theme.breakpoints.down("1025")]: {
+        fontSize: "10px",
+      },
+    },
+    data: {
+      fontSize: "17px",
+      fontWeight: "600",
+      marginBottom: "8px",
+      [theme.breakpoints.down("1025")]: {
+        fontSize: "16px",
+      },
+    },
+    growth: {
+      [theme.breakpoints.between("481", "900")]: {
+        paddingLeft: "0px",
+        marginLeft: "-10px",
+      },
+    },
+    overall: {
+      display: "flex",
+      backgroundColor: "#1F3625",
+      marginTop: "7%",
+      color: "#ffffff",
+      fontWeight: "350",
+      minHeight: "50px",
+      borderRadius: "10px",
+      maxWidth: "180px",
+      [theme.breakpoints.down("1500")]: {
+        minHeight: "40px",
+      },
+    },
+    done: {
+      backgroundColor: "#9BC53D",
+      borderRadius: "10px",
+      alignItems: "center",
+      fontWeight: "400",
+      fontSize: "25px",
+    },
+    count: {
+      fontWeight: "450",
+      position: "absolute",
+      marginLeft: "0.25em",
+      marginTop: "0.25em",
+      fontSize: "25px",
+      display: "flex",
+    },
+    countdesc: {
+      fontSize: "11px",
+      textAlign: "center",
+      maxWidth: "150px",
+      [theme.breakpoints.down("1025")]: {
+        fontSize: "9px",
+      },
+    },
+    imageWindow: {
+      height: "auto",
+      borderRadius: "20px",
+      objectFit: "cover",
+      padding: "2%",
+      width: "auto",
+      maxWidth: "540px",
+    },
+  })
 );
