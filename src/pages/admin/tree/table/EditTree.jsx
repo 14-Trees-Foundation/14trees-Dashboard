53bdd060c75d0c3ad9065f0464fcdc59cf874e56
Mounesha Zonde <mouneshe2002@gmail.com>
2024-05-23 20:17:36 +0530

    edit tree

---
+++ src/pages/admin/tree/table/EditTree.jsx
@@ -0,0 +1,163 @@
+import React, { useState } from 'react'
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Modal,
+    TextField,
+    Typography,
+} from "@mui/material";
+
+function EditTree({ row, openeditModal, setEditModal, editSubmit }) {
+
+    const [formData, setFormData] = useState(row);
+    console.log(formData);
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prevState => {
+            if (name === 'tree_id.name') {
+                return { ...prevState, tree_id: { ...prevState.tree_id, name: value } };
+            } else if (name.includes('_')) {
+                const [parent, child] = name.split('_');
+                return { ...prevState, [parent]: { ...prevState[parent], [child]: value } };
+            }
+            return { ...prevState, [name]: value };
+        });
+    };
+
+    const handleEditSubmit = (event) => {
+        event.preventDefault();
+        editSubmit(formData);
+        setEditModal(false);
+    };
+
+    return (
+        <Dialog open={openeditModal} onClose={() => setEditModal(false)}>
+            <DialogTitle align="center">Edit Tree</DialogTitle>
+            <form onSubmit={handleEditSubmit}>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="sapling_id"
+                        label="Sapling ID"
+                        type="text"
+                        fullWidth
+                        value={formData.sapling_id}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="tree_id._id"
+                        label="Tree ID"
+                        type="text"
+                        fullWidth
+                        value={formData.tree_id._id}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="tree_id.name"
+                        label="Tree Name"
+                        type="text"
+                        fullWidth
+                        value={formData.tree_id.name}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        name="plot_id._id"
+                        label="Plot ID"
+                        type="text"
+                        fullWidth
+                        value={formData.plot_id._id}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        name="plot_id.name"
+                        label="Plot Name"
+                        type="text"
+                        fullWidth
+                        value={formData.plot_id.name}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        name="date_added"
+                        label="Date Added"
+                        type="text"
+                        fullWidth
+                        value={formData.date_added}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="key"
+                        label="Key"
+                        type="text"
+                        fullWidth
+                        value={formData.key}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="link"
+                        label="Link"
+                        type="text"
+                        fullWidth
+                        value={formData.link}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="mapped_to"
+                        label="Mapped To"
+                        type="text"
+                        fullWidth
+                        value={formData.mapped_to}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="event_type"
+                        label="Event Type"
+                        type="text"
+                        fullWidth
+                        value={formData.event_type}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="date_assigned"
+                        label="Date Assigned"
+                        type="text"
+                        fullWidth
+                        value={formData.date_assigned}
+                        onChange={handleChange}
+                    />
+                </DialogContent>
+                <DialogActions sx={{ display: 'flex', justifyContent: 'center', marginBottom: '15px' }}>
+                    <Button variant='contained' onClick={() => setEditModal(false)} color="primary">
+                        Cancel
+                    </Button>
+                    <Button variant='contained' type="submit" color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </form>
+        </Dialog>
+    )
+}
+
+export default EditTree
--- src/pages/admin/tree/table/Tree.tsx
+++ src/pages/admin/tree/table/Tree.tsx
@@ -1,175 +1,175 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, GridToolbar, GridColumns, GridFilterItem, GridFilterModel } from '@mui/x-data-grid';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal, Typography } from "@mui/material";
-import AddTree from './AddTree';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { type Tree } from '../../../../types/tree';
-import * as treeActionCreators from '../../../../redux/actions/treeActions';
-import { bindActionCreators } from 'redux';
-import { useAppDispatch, useAppSelector } from '../../../../redux/store/hooks';
-import { RootState } from '../../../../redux/store/store';
-import CircularProgress from '@mui/material/CircularProgress';
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import {
+    DataGrid,
+    GridToolbar,
+    GridColumns,
+    GridFilterItem,
+    GridFilterModel,
+} from "@mui/x-data-grid";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Modal,
+    Typography,
+} from "@mui/material";
+import AddTree from "./AddTree";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { type Tree } from "../../../../types/tree";
+import * as treeActionCreators from "../../../../redux/actions/treeActions";
+import { bindActionCreators } from "redux";
+import { useAppDispatch, useAppSelector } from "../../../../redux/store/hooks";
+import { RootState } from "../../../../redux/store/store";
+import CircularProgress from "@mui/material/CircularProgress";
+import EditTree from "./EditTree";
 
 function LoadingOverlay() {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+            }}>
             <CircularProgress />
         </div>
     );
 }
 
-
 export const TreeNew = () => {
-
     const dispatch = useAppDispatch();
-    const { getTrees, createTree, updateTree, deleteTree, createBulkTrees }
-        = bindActionCreators(treeActionCreators, dispatch);
+    const { getTrees, createTree, updateTree, deleteTree, createBulkTrees } =
+        bindActionCreators(treeActionCreators, dispatch);
 
     const [open, setOpen] = useState(false);
     const handleModalOpen = () => setOpen(true);
     const handleModalClose = () => setOpen(false);
     const [openDeleteModal, setOpenDeleteModal] = useState(false);
     const [deleteRow, setDeleteRow] = useState<any>({});
-    const [ filters, setFilters ] = useState<GridFilterItem[]>([]);
+    const [filters, setFilters] = useState<GridFilterItem[]>([]);
     const [page, setPage] = useState(0);
+    const [selectedEditRow, setSelectedEditRow] = useState<Tree | null>(null);
+    const [editModal, setEditModal] = useState(false);
 
     useEffect(() => {
         getTreeData();
-    }, [page, filters]);
+    }, [page, filters, editModal]);
 
     const getTreeData = async () => {
         setTimeout(async () => {
-            await getTrees(page*10, 10, filters);
+            await getTrees(page * 10, 10, filters);
         }, 1000);
     };
-    
+
     const columns: GridColumns = [
         {
-            field: 'action',
-            headerName: 'Action',
+            field: "action",
+            headerName: "Action",
             width: 100,
-            align: 'center',
-            headerAlign: 'center',
+            align: "center",
+            headerAlign: "center",
             renderCell: (params: any) => (
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <Button  onClick={() => console.log('Edit', params.row)}><EditIcon /></Button>
-                    <Button  onClick={() => handleDelete(params.row)}><DeleteIcon /></Button>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}>
+                    <Button
+                        onClick={() => {
+                            setSelectedEditRow(params.row);
+                            setEditModal(true);
+                        }}>
+                        <EditIcon />
+                    </Button>
+                    <Button onClick={() => handleDelete(params.row)}>
+                        <DeleteIcon />
+                    </Button>
                 </div>
             ),
         },
-        { field: '_id', headerName: 'ID', width: 90, align: 'center', headerAlign: 'center' },
-        {
-            field: 'sapling_id',
-            headerName: 'Sapling ID',
-            width: 150, align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: 'tree_id',
-            headerName: 'Tree Type',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center',
-            valueGetter: (params: any) => params.row?.tree_id.name
-        },
-        {
-            field: 'plot_id',
-            headerName: 'Plot Name',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center',
-            valueGetter: (params: any) => params.row?.plot_id.name
-        },
-        {
-            field: 'date_added',
-            headerName: 'Date Added',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: 'link',
-            headerName: 'Link',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: 'mapped_to',
-            headerName: 'Mapped To',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: 'event_type',
-            headerName: 'Event Type',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: 'date_assigned',
-            headerName: 'Date Assigned',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: '__v',
-            headerName: '__V',
-            width: 90,
-            align: 'center',
-            headerAlign: 'center'
-        },
-        {
-            field: 'location.type',
-            headerName: 'Location Type',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center',
-            valueGetter: (params) => params.row.location?.type,
-        },
-        {
-            field: 'location.coordinates',
-            headerName: 'Location Coordinates',
-            width: 150,
-            align: 'center',
-            headerAlign: 'center',
-            valueGetter: (params) => params.row.location?.coordinates.join(', '),
-        },
-        
+        { field: "sapling_id", headerName: "Sapling ID", width: 150 },
+        { field: "tree_id._id", headerName: "Tree ID", width: 150 },
+        { field: "tree_id.name", headerName: "Tree Name", width: 150 },
+        { field: "plot_id._id", headerName: "Plot ID", width: 150 },
+        { field: "plot_id.name", headerName: "Plot Name", width: 150 },
+        { field: "date_added", headerName: "Date Added", width: 150 },
+        { field: "key", headerName: "Key", width: 150 },
+        { field: "link", headerName: "Link", width: 150 },
+        { field: "mapped_to", headerName: "Mapped To", width: 150 },
+        { field: "event_type", headerName: "Event Type", width: 150 },
+        { field: "date_assigned", headerName: "Date Assigned", width: 150 },
     ];
 
     let treesList: Tree[] = [];
     const treesData = useAppSelector((state: RootState) => state.treesData);
     if (treesData) {
-        treesList = Object.values(treesData.trees);
+        treesList = Object.values(treesData.trees).map(tree => ({
+            ...tree,
+            'tree_id._id': tree.tree_id._id,
+            'tree_id.name': tree.tree_id.name,
+            'plot_id._id': tree.plot_id._id,
+            'plot_id.name': tree.plot_id.name,
+        }));
     }
 
+    const unflattenTree = (formData: any) => ({
+        ...formData,
+        tree_id: {
+            _id: formData['tree_id._id'],
+            name: formData['tree_id.name']
+        },
+        plot_id: {
+            _id: formData['plot_id._id'],
+            name: formData['plot_id.name']
+        }
+    });
+
+    console.log(treesList);
     type RowType = {
         id: string;
         name: string;
     };
 
     const handleDelete = (row: RowType) => {
-        console.log('Delete', row);
+        console.log("Delete", row);
         setOpenDeleteModal(true);
         setDeleteRow(row);
     };
 
+    const handleEditSubmit = (formData: Tree) => {
+        
+        console.log(formData);
+        updateTree(formData as Tree);
+    };
+
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '20px' }}>
-                <Button variant="contained"  onClick={handleModalOpen}
-                disabled={true} 
-                >Add Tree</Button>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    marginBottom: "20px",
+                }}>
+                <Button variant="contained" onClick={handleModalOpen} disabled={true}>
+                    Add Tree
+                </Button>
                 <AddTree open={open} handleClose={handleModalClose} />
-                <Button variant="contained" style={{ marginLeft: '10px' }} onClick={handleModalOpen} disabled={true} >Bulk Create</Button>
+                <Button
+                    variant="contained"
+                    style={{ marginLeft: "10px" }}
+                    onClick={handleModalOpen}
+                    disabled={true}>
+                    Bulk Create
+                </Button>
             </div>
-            <Box sx={{ height: 540, width: '100%' }}>
+            <Box sx={{ height: 540, width: "100%" }}>
                 <DataGrid
                     rows={treesList}
                     columns={columns}
@@ -177,15 +177,17 @@ export const TreeNew = () => {
                     initialState={{
                         pagination: {
                             page: 0,
-                            pageSize: 10
+                            pageSize: 10,
                         },
                     }}
-                    onPageChange={(page) => { if((treesList.length / 10) === page) setPage(page); }}
+                    onPageChange={(page) => {
+                        if (treesList.length / 10 === page) setPage(page);
+                    }}
                     rowCount={treesData.totalTrees}
                     onFilterModelChange={(model: GridFilterModel) => {
-                        setFilters(model.items)
+                        setFilters(model.items);
                     }}
-                    filterMode='server'
+                    filterMode="server"
                     checkboxSelection
                     disableSelectionOnClick
                     components={{
@@ -199,7 +201,8 @@ export const TreeNew = () => {
                 <DialogTitle>Confirm Delete</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        Do you want to delete tree with the sapling id '{deleteRow?.sapling_id}'?
+                        Do you want to delete tree with the sapling id '
+                        {deleteRow?.sapling_id}'?
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
@@ -218,6 +221,14 @@ export const TreeNew = () => {
                 </DialogActions>
             </Dialog>
 
+            {selectedEditRow && (
+                <EditTree
+                    row={selectedEditRow}
+                    openeditModal={editModal}
+                    setEditModal={setEditModal}
+                    editSubmit={handleEditSubmit}
+                />
+            )}
         </>
     );
-}
+};
