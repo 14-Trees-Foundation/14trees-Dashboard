fb043ad12b5c4097e3043c8a623f71bc09c7206f
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-09-18 18:50:58 +0530

    inventory management: improments on sites map

--- src/pages/admin/Inventory/SiteMap.tsx
+++ src/pages/admin/Inventory/SiteMap.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { GoogleMap, LoadScript, KmlLayer } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, KmlLayer, Polygon } from '@react-google-maps/api';
+import { Plot } from '../../../types/plot';
 
 const containerStyle = {
     width: '100%',
     height: '500px',
 };
 
-interface KmlLayerProps {
-    url: string;
+const lightGreen = "#00ff00";
+const lightRed = "#ff0000";
+const lightOrange = "#ffa500";
+
+const defaultPolygonOptions = {
+    fillColor: lightGreen,
+    fillOpacity: 0.1,
+    strokeColor: 'black',
+    strokeOpacity: 1,
+    strokeWeight: 2,
+    clickable: false,
+    editable: false,
+    visible: true,
+    geodesic: false,
+    zIndex: 1,
 }
 
-const MapWithKmlLayer: React.FC<KmlLayerProps> = ({ url }) => {
+interface SitesMapProps {
+    plots: Plot[]
+}
+
+
+const SitesMap: React.FC<SitesMapProps> = ({ plots }) => {
     const googleMapsApiKey = process.env.REACT_APP_API_MAP_KEY ?? '';
+
+    const getPlotPolygon = (plot: Plot) => {
+        const polygonPath = plot.boundaries?.coordinates[0]?.map(
+            ([lat, lng]) => ({ lat, lng })
+        );
+        return polygonPath;
+    };
 
     return (
         <LoadScript googleMapsApiKey={googleMapsApiKey}>
@@ -20,11 +46,12 @@ const MapWithKmlLayer: React.FC<KmlLayerProps> = ({ url }) => {
                 zoom={10}
                 mapTypeId='satellite'
             >
-                {/* KML Layer */}
-                <KmlLayer url={url} />
+                {plots.map((plot) => (
+                    <Polygon key={plot.id} path={getPlotPolygon(plot)} options={defaultPolygonOptions} />
+                ))}
             </GoogleMap>
         </LoadScript>
     );
 };
 
-export default MapWithKmlLayer;
+export default SitesMap;
