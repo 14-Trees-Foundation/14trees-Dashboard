08253e461e145c5f83d9036423e51bc08e2ea44a
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-09-18 19:13:06 +0530

    inventory management: improments on sites map

--- src/pages/admin/Inventory/SiteMap.tsx
+++ src/pages/admin/Inventory/SiteMap.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript, KmlLayer, Polygon } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, LoadScript, Marker, Polygon } from '@react-google-maps/api';
 import { Plot } from '../../../types/plot';
 
 const containerStyle = {
@@ -10,9 +10,10 @@ const containerStyle = {
 const lightGreen = "#00ff00";
 const lightRed = "#ff0000";
 const lightOrange = "#ffa500";
+const lightBlue = "#0000ff";
 
 const defaultPolygonOptions = {
-    fillColor: lightGreen,
+    fillColor: lightBlue,
     fillOpacity: 0.1,
     strokeColor: 'black',
     strokeOpacity: 1,
@@ -39,15 +40,66 @@ const SitesMap: React.FC<SitesMapProps> = ({ plots }) => {
         return polygonPath;
     };
 
+    const calculatePlotCenter = (plot: Plot) => {
+        let latSum = 0, lngSum = 0;
+        const path = getPlotPolygon(plot);
+        path.forEach(point => {
+            latSum += point.lat;
+            lngSum += point.lng;
+        });
+        return {
+            lat: latSum / path.length,
+            lng: lngSum / path.length,
+        };
+    };
+
+    const getPolygonOptions = (plot: Plot) => {
+        switch (plot.accessibility_status) {
+            case 'accessible':
+                return {
+                    ...defaultPolygonOptions,
+                    fillColor: lightGreen
+                }
+            case 'inaccessible':
+                return {
+                    ...defaultPolygonOptions,
+                    fillColor: lightRed
+                }
+            case 'moderately_accessible':
+                return {
+                    ...defaultPolygonOptions,
+                    fillColor: lightOrange
+                }
+            default:
+                return defaultPolygonOptions
+        }
+    }
+
     return (
         <LoadScript googleMapsApiKey={googleMapsApiKey}>
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 zoom={10}
                 mapTypeId='satellite'
+                center={{ lat: 0, lng: 0 }}
             >
                 {plots.map((plot) => (
-                    <Polygon key={plot.id} path={getPlotPolygon(plot)} options={defaultPolygonOptions} />
+                    <Polygon key={plot.id} path={getPlotPolygon(plot)} options={getPolygonOptions(plot)} />
+                ))}
+
+                {plots.map((plot) => (
+                    <Marker key={plot.id} position={calculatePlotCenter(plot)} options={{ label: plot.label }}/>
+                ))}
+
+                {plots.map((plot) => (
+                    <InfoWindow
+                        position={calculatePlotCenter(plot)}
+                    >
+                        <div>
+                            <h4>{plot.label}</h4>
+                            <p>area: {plot.acres_area}</p>
+                        </div>
+                    </InfoWindow>
                 ))}
             </GoogleMap>
         </LoadScript>
