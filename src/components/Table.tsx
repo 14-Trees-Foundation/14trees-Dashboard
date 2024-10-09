eae9d466f875c548f7a4d6efce57a4f5be8819ff
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-10-09 18:08:05 +0530

    improvements on inventory

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@mui/material';
-import { Table, TableColumnsType } from 'antd';
+import { Settings } from '@mui/icons-material';
+import { Box, Button, Divider, IconButton } from '@mui/material';
+import { Checkbox, Dropdown, MenuProps, Table, TableColumnsType } from 'antd';
 import { AnyObject } from 'antd/es/_util/type';
 import { TableRowSelection } from 'antd/es/table/interface';
 import { Parser } from 'json2csv';
 import { ReactElement, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { Resizable } from "react-resizable";
 
 interface TableComponentProps {
     loading?: boolean
@@ -21,10 +23,48 @@ interface TableComponentProps {
         ReactElement
 }
 
+const ResizableTitle = (props: any) => {
+    const { onResize, width, ...restProps } = props;
+
+    if (!width) {
+        return <th {...restProps} />;
+    }
+
+    return (
+        <Resizable
+            width={width}
+            height={0}
+            handle={
+                <span
+                    style={{
+                        "position": "absolute",
+                        "right": "-5px",
+                        "bottom": 0,
+                        // "z-index": 1,
+                        "width": "10px",
+                        "height": "100%",
+                        "cursor": "col-resize",
+                    }}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                />
+            }
+            onResize={onResize}
+            draggableOpts={{ enableUserSelectHack: false }}
+        >
+            <th {...restProps} />
+        </Resizable>
+    );
+};
+
 function TableComponent({ loading, dataSource, columns, totalRecords, fetchAllData, setPageSize, setPage, handleSelectionChanges, setSrNoPage, isExpandable, expandableFunction }: TableComponentProps) {
 
     const [download, setDownload] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+    const [checkedList, setCheckedList] = useState(columns?.map((item) => item.key) ?? []);
+    const [open, setOpen] = useState(false);
+    const [tableCols, setTableCols] = useState<any[]>([]);
 
     let rowSelection: TableRowSelection<AnyObject> | undefined;
     if (handleSelectionChanges) {
@@ -88,6 +128,13 @@ function TableComponent({ loading, dataSource, columns, totalRecords, fetchAllDa
         if (loading !== undefined) setIsLoading(loading);
     }, [loading]);
 
+    useEffect(() => {
+        setTableCols(columns?.map((item: any) => ({
+            ...item,
+            hidden: !checkedList.includes(item.key as string),
+        })) ?? []);
+    }, [columns, checkedList]);
+
     const handlePageChange = (page: number, pageSize: number) => {
         if (dataSource && page * pageSize > dataSource.length) {
             const pageNo = Math.floor(dataSource.length / pageSize);
@@ -100,12 +147,57 @@ function TableComponent({ loading, dataSource, columns, totalRecords, fetchAllDa
         }
     }
 
+    const handleOpenChange = (flag: boolean, info: { source: 'menu' | 'trigger' }) => {
+        if (info.source === 'trigger') setOpen(flag);
+    };
+
+    const handleColumnsSelection = (key: string) => {
+        const newSelected = checkedList.includes(key) ? checkedList.filter((item) => item !== key) : [...checkedList, key];
+        setCheckedList(newSelected);
+    }
+
+    const newColumns = columns?.map((item) => ({
+        ...item,
+        hidden: !checkedList.includes(item.key as string),
+    }));
+
+    const items: MenuProps['items'] = columns?.map((column: any) => {
+        const title = typeof column.title === 'string' ? column.title : column.dataIndex;
+        return {
+            key: column.dataIndex,
+            label: <Checkbox checked={checkedList.includes(column.key)} onChange={() => handleColumnsSelection(column.key)}>{title}</Checkbox>
+        }
+    })
+
+    const components = {
+        header: {
+            cell: ResizableTitle
+        }
+    };
+
+    const handleResize = (index: number) => (e: any, info: { size: any }) => {
+        setTableCols(prev => {
+            const nextColumns = [...prev];
+            nextColumns[index] = {
+                ...nextColumns[index],
+                width: info.size.width
+            };
+            return newColumns as any[];
+        });
+    };
+
     return (
         <Table
             loading={isLoading}
             style={{ borderRadius: 20 }}
             dataSource={dataSource}
-            columns={columns}
+            columns={tableCols.map((col, index) => ({
+                ...col,
+                onHeaderCell: (column: any) => ({
+                    width: column.width,
+                    onResize: handleResize(index)
+                })
+            }))}
             expandable={isExpandable ? expandable : undefined}
             pagination={{
                 position: ['bottomRight'],
@@ -114,10 +206,21 @@ function TableComponent({ loading, dataSource, columns, totalRecords, fetchAllDa
                 simple: true,
                 onChange: handlePageChange,
             }}
+            components={components}
             rowSelection={rowSelection}
             scroll={{ y: 550 }}
             footer={() => (
                 <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Dropdown
+                        menu={{ items }}
+                        placement="bottomLeft"
+                        open={open}
+                        onOpenChange={handleOpenChange}
+                        trigger={['click']}
+                    >
+                        <IconButton sx={{ marginLeft: 'auto' }}><Settings /></IconButton>
+                    </Dropdown>
+                    <Divider orientation="vertical" flexItem sx={{ backgroundColor: "black", marginRight: '10px', }} />
                     <div style={{ marginRight: '10px', display: 'flex', alignItems: 'center' }}><strong>Export table data in a csv file:</strong></div>
                     <Button
                         color="success"
