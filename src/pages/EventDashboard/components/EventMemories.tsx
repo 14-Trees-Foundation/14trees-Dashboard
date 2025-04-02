d194d9b7c895852fec00b5b094417baeb03a0aba
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-04-02 17:50:29 +0530

    Event dashbaords: improvements

--- src/pages/EventDashboard/components/EventMemories.tsx
+++ src/pages/EventDashboard/components/EventMemories.tsx
@@ -1,30 +1,43 @@
 import React from "react";
 import { Carousel } from "antd";
+import { useMediaQuery } from "@mui/material";
 
 interface ImageCarouselProps {
     imageUrls: string[];
 }
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ imageUrls }) => {
+    const isMobile = useMediaQuery("(max-width:600px)");
+
     return (
-        <div style={{ width: "100%", maxWidth: "80vw", height: "420px", overflow: "hidden" }}>
+        <div
+            style={{
+                width: "100%",
+                maxWidth: isMobile ? "96vw" : "80vw", // Full width for mobile, 80vw for larger screens
+                height: isMobile ? "250px" : "420px", // Adjust height for mobile
+                overflow: "hidden",
+            }}
+        >
             <Carousel autoplay dots style={{ width: "100%", height: "100%" }}>
                 {imageUrls.map((url, index) => (
                     <div key={index}>
                         <div
                             style={{
-                                display: 'flex',
-                                justifyContent: 'center',
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
                                 height: "100%",
-                                width: '100%',
+                                width: "100%",
                             }}
                         >
                             <img
                                 src={url}
                                 alt={`Slide ${index}`}
                                 style={{
-                                    height: '400px',
+                                    height: isMobile ? "230px" : "400px",
                                     width: 'auto',
+                                    // maxHeight: "100%",
+                                    // maxWidth: "100%",
                                     objectFit: "contain",
                                 }}
                             />
@@ -37,4 +50,3 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ imageUrls }) => {
 };
 
 export default ImageCarousel;
-
