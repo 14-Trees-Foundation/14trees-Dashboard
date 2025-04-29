6f4f5876e0226a2e50a3f256b9b4e2f8c51954ad
elishaa31 <elishaaa31@gmail.com>
2025-04-29 13:58:18 +0530

    Demo web page

---
+++ src/pages/chatbot/Chatbot.tsx
@@ -0,0 +1,15 @@
+// src/pages/Chatbot/Chatbot.jsx
+import React from 'react';
+import ChatbotV2 from '../../components/Chatbot/ChatBotV2';
+
+const Chatbot = () => {
+  return (
+    <div>
+      <h1>Chatbot Page</h1>
+      <p>This is a blank page for the Chatbot.</p>
+      <ChatbotV2 />
+    </div>
+  );
+};
+
+export default Chatbot;
