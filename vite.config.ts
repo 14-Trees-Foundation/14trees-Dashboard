40ee47d2955df11f55138a6749defa0291133057
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-04-30 10:39:46 +0530

    Vite: build output folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    build: {
+        outDir: 'build',
+    },
     server: {
         open: true,
         port: 3000,
