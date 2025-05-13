33bf0f79b2439f0146472afda1be6c8569d3682d
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-05-13 13:55:11 +0530

    Gift Messages: single primary message for gift cards

--- src/pages/admin/gift/Form/CardDetailsForm.tsx
+++ src/pages/admin/gift/Form/CardDetailsForm.tsx
@@ -2,17 +2,18 @@ import { FC, useEffect, useRef, useState } from "react";
 import { Box, Button, CircularProgress, TextField, Typography, useMediaQuery, useTheme } from "@mui/material";
 import ApiClient from "../../../../api/apiClient/apiClient";
 
+const prefixMessage = "Dear {recipient},\n\n"
+const secondaryMessage = 'We invite you to visit 14 Trees and firsthand experience the growth and contribution of your tree towards a greener future.';
+
 const defaultMessages = {
-    primary: 'We are immensely delighted to share that a tree has been planted in your name at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, rejuvenating ecosystems, supporting biodiversity, and helping offset the harmful effects of climate change.',
-    birthday: 'We are immensely delighted to share that a tree has been planted in your name on the occasion of your birthday at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, helping offset the harmful effects of climate change.',
-    memorial: 'A tree has been planted in the memory of <name here> at the 14 Trees Foundation reforestation site. For many years, this tree will help rejuvenate local ecosystems, support local biodiversity and offset the harmful effects of climate change and global warming.',
-    secondary: 'We invite you to visit 14 Trees and firsthand experience the growth and contribution of your tree towards a greener future.',
+    primary: prefixMessage + 'We are immensely delighted to share that a tree has been planted in your name at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, rejuvenating ecosystems, supporting biodiversity, and helping offset the harmful effects of climate change.' + "\n\n" + secondaryMessage,
+    birthday: prefixMessage + 'We are immensely delighted to share that a tree has been planted in your name on the occasion of your birthday at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, helping offset the harmful effects of climate change.' + "\n\n" + secondaryMessage,
+    memorial: prefixMessage + 'A tree has been planted in the memory of <name here> at the 14 Trees Foundation reforestation site. For many years, this tree will help rejuvenate local ecosystems, support local biodiversity and offset the harmful effects of climate change and global warming.' + "\n\n" + secondaryMessage,
     logo: 'Gifted by 14 Trees in partnership with'
 }
 
 interface Massages {
     primaryMessage: string,
-    secondaryMessage: string,
     eventName: string,
     eventType: string | undefined,
     plantedBy: string,
@@ -40,16 +41,17 @@ const CardDetails: FC<CardDetailsProps> = ({ logo_url, request_id, presentationI
     const isMobile = useMediaQuery(theme.breakpoints.down('md'));
     const slideIdRef = useRef('');
     const presentationIdIdRef = useRef('');
-    const recordRef = useRef({ primary: '', secondary: '', logo: '' })
+    const recordRef = useRef({ primary: '', logo: '' })
     const logoRef = useRef({ logoUrl: undefined as string | null | undefined })
     const [iframeSrc, setIframeSrc] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
-    const giftRef = useRef({ 
-        saplingId: undefined as string | null | undefined, 
-        plantType: undefined as string | null | undefined, 
+    const giftRef = useRef({
+        saplingId: undefined as string | null | undefined,
+        plantType: undefined as string | null | undefined,
         userName: undefined as string | null | undefined,
         treesCount: undefined as number | undefined,
     })
+    const [msgError, setMsgError] = useState<string>("");
 
     const updateSlide = async () => {
         if (!slideIdRef.current || !presentationIdIdRef.current) {
@@ -58,7 +60,7 @@ const CardDetails: FC<CardDetailsProps> = ({ logo_url, request_id, presentationI
 
         setLoading(true);
         const apiClient = new ApiClient();
-        await apiClient.updateGiftCardTemplate(slideIdRef.current, recordRef.current.primary, recordRef.current.secondary, recordRef.current.logo, logoRef.current.logoUrl, giftRef.current.saplingId, giftRef.current.userName, giftRef.current.treesCount);
+        await apiClient.updateGiftCardTemplate(slideIdRef.current, recordRef.current.primary, recordRef.current.logo, logoRef.current.logoUrl, giftRef.current.saplingId, giftRef.current.userName, giftRef.current.treesCount);
         setIframeSrc(
             `https://docs.google.com/presentation/d/${presentationIdIdRef.current}/embed?rm=minimal&slide=id.${slideIdRef.current}&timestamp=${new Date().getTime()}`
         );
@@ -69,7 +71,7 @@ const CardDetails: FC<CardDetailsProps> = ({ logo_url, request_id, presentationI
         const generateGiftCard = async () => {
             setLoading(true);
             const apiClient = new ApiClient();
-            const resp = await apiClient.generateCardTemplate(request_id, defaultMessages.primary, defaultMessages.secondary, defaultMessages.logo, logoRef.current.logoUrl, giftRef.current.saplingId, giftRef.current.userName, giftRef.current.plantType, isPersonal);
+            const resp = await apiClient.generateCardTemplate(request_id, defaultMessages.primary, defaultMessages.logo, logoRef.current.logoUrl, giftRef.current.saplingId, giftRef.current.userName, giftRef.current.plantType, isPersonal);
             slideIdRef.current = resp.slide_id;
             presentationIdIdRef.current = resp.presentation_id;
 
@@ -107,24 +109,24 @@ const CardDetails: FC<CardDetailsProps> = ({ logo_url, request_id, presentationI
 
     useEffect(() => {
         const eventMessage = messages.eventType === "2" ? defaultMessages.memorial : messages.eventType === "1" ? defaultMessages.birthday : defaultMessages.primary;
-        if (messages.primaryMessage === "" || messages.secondaryMessage === "" || messages.logoMessage === ""
+        if (messages.primaryMessage === "" || messages.logoMessage === ""
             || ((messages.primaryMessage === defaultMessages.primary || messages.primaryMessage === defaultMessages.birthday || messages.primaryMessage === defaultMessages.memorial) && messages.primaryMessage !== eventMessage)) {
             onChange({
                 ...messages,
                 primaryMessage: eventMessage,
-                secondaryMessage: defaultMessages.secondary,
                 logoMessage: defaultMessages.logo,
             })
 
             recordRef.current.primary = eventMessage;
-            recordRef.current.secondary = defaultMessages.secondary;
             recordRef.current.logo = defaultMessages.logo;
 
             updateSlide();
         } else {
             recordRef.current.primary = messages.primaryMessage;
-            recordRef.current.secondary = messages.secondaryMessage;
             recordRef.current.logo = messages.logoMessage;
+
+            if (!messages.primaryMessage.includes("{recipient}")) setMsgError("Didn't found \"{recipient}\" placeholder text in your message. Recipient's name will not be visible in the generate tree card");
+            else setMsgError("");
         }
     }, [messages])
 
@@ -139,43 +141,34 @@ const CardDetails: FC<CardDetailsProps> = ({ logo_url, request_id, presentationI
     }
 
     return (
-        <div style={{ 
-            display: 'flex', 
+        <div style={{
+            display: 'flex',
             flexDirection: isMobile ? 'column' : 'row',
-            padding: isMobile ? '0px' : '10px 10px', 
-            width: '100%', 
+            padding: isMobile ? '0px' : '10px 10px',
+            width: '100%',
             justifyContent: 'space-between',
             gap: isMobile ? '20px' : '0'
         }}>
-            <div style={{ 
-                display: 'flex', 
-                flexDirection: 'column', 
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
                 width: isMobile ? '100%' : '42%'
             }}>
                 <Typography variant='h6'>{messages.eventType === "1" ? "Birthday" : messages.eventType === "2" ? "Memorial" : "General"} Gift</Typography>
                 <Typography variant='body1' mt={1}>If you would like to tweak/add some personalised touch, change the messaging below: </Typography>
-                <Typography variant="body1" sx={{ mt: 2 }}><strong>Primary Message</strong></Typography>
+                <Typography variant="body1" sx={{ mt: 2 }}><strong>Card Message</strong></Typography>
                 <TextField
                     multiline
                     name="primaryMessage"
                     value={messages.primaryMessage}
                     onChange={handleChange}
                     size="small"
-                    inputProps={{ maxLength: 270 }}
+                    inputProps={{ maxLength: 430 }}
                     FormHelperTextProps={{ style: { textAlign: 'right' } }}
-                    helperText={`${270 - messages.primaryMessage.length} characters remaining (max: 270)`}
+                    helperText={`${430 - messages.primaryMessage.length} characters remaining (max: 430)`}
+                    error={msgError !== ""}
                 />
-                <Typography variant="body1" sx={{ mt: 2 }}><strong>Secondary Message</strong></Typography>
-                <TextField
-                    multiline
-                    name="secondaryMessage"
-                    value={messages.secondaryMessage}
-                    onChange={handleChange}
-                    size="small"
-                    inputProps={{ maxLength: 125 }}
-                    FormHelperTextProps={{ style: { textAlign: 'right' } }}
-                    helperText={`${125 - messages.secondaryMessage.length} characters remaining (max: 125)`}
-                />
+                {msgError !== "" && <Typography mt={1} ml={1} color='crimson' variant='body2'>Warning: {msgError}</Typography>}
                 {logo_url && <Box>
                     <Typography variant="body1" sx={{ mt: 2 }}>Logo Message</Typography>
                     <TextField
