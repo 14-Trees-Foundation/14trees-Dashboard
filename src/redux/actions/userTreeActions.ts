841e79d60dbbf26f3e1f386844e0ae84ac09bd53
ab-si <abhisingh7294@gmail.com>
2026-04-16 11:32:57 +0530

    Remove unused code , user tree

--- src/redux/actions/userTreeActions.ts
+++ src/redux/actions/userTreeActions.ts
@@ -1,228 +1,205 @@
-import ApiClient from "../../api/apiClient/apiClient";
-import userTreeActionTypes from "../actionTypes/userTreeActionTypes";
-import { AssignTreeRequest, UserTree, UserTreeCountPaginationResponse } from "../../types/userTree";
-import { toast } from "react-toastify";
-import { MapTreesUsingPlotIdRequest, MapTreesUsingSaplingIdsRequest } from "../../types/tree";
+import ApiClient from '../../api/apiClient/apiClient';
+import userTreeActionTypes from '../actionTypes/userTreeActionTypes';
+import { AssignTreeRequest, UserTree } from '../../types/userTree';
+import { toast } from 'react-toastify';
+import {
+	MapTreesUsingPlotIdRequest,
+	MapTreesUsingSaplingIdsRequest,
+} from '../../types/tree';
 
 export const getUserTrees = () => {
-    const apiClient = new ApiClient()
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.GET_USER_TREES_REQUESTED,
-        });
-        apiClient.getUserTrees().then(
-            (value: UserTree[]) => {
-                for (let i = 0; i < value.length; i++) {
-                    if (value[i]?._id) {
-                        value[i].key = value[i]._id
-                    }
-                }
-                dispatch({
-                    type: userTreeActionTypes.GET_USER_TREES_SUCCEEDED,
-                    payload: value,
-                });
-            },
-            (error: any) => {
-                console.log(error)
-                dispatch({
-                    type: userTreeActionTypes.GET_USER_TREES_FAILED,
-                    payload: error
-                });
-            }
-        )
-    }
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.GET_USER_TREES_REQUESTED,
+		});
+		apiClient.getUserTrees().then(
+			(value: UserTree[]) => {
+				for (let i = 0; i < value.length; i++) {
+					if (value[i]?._id) {
+						value[i].key = value[i]._id;
+					}
+				}
+				dispatch({
+					type: userTreeActionTypes.GET_USER_TREES_SUCCEEDED,
+					payload: value,
+				});
+			},
+			(error: any) => {
+				console.log(error);
+				dispatch({
+					type: userTreeActionTypes.GET_USER_TREES_FAILED,
+					payload: error,
+				});
+			},
+		);
+	};
 };
 
 export const createUserTree = (record: UserTree) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.CREATE_USER_TREE_REQUESTED,
-        });
-        apiClient.createUserTree(record).then(
-            (value: UserTree) => {
-                dispatch({
-                    type: userTreeActionTypes.CREATE_USER_TREE_SUCCEEDED,
-                    payload: value,
-                });
-            },
-            (error: any) => {
-                console.error(error);
-                dispatch({
-                    type: userTreeActionTypes.CREATE_USER_TREE_FAILED,
-                });
-            }
-        )
-    };
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.CREATE_USER_TREE_REQUESTED,
+		});
+		apiClient.createUserTree(record).then(
+			(value: UserTree) => {
+				dispatch({
+					type: userTreeActionTypes.CREATE_USER_TREE_SUCCEEDED,
+					payload: value,
+				});
+			},
+			(error: any) => {
+				console.error(error);
+				dispatch({
+					type: userTreeActionTypes.CREATE_USER_TREE_FAILED,
+				});
+			},
+		);
+	};
 };
 
 export const updateUserTree = (record: UserTree) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.UPDATE_USER_TREE_REQUESTED,
-        });
-        apiClient.updateUserTree(record).then(
-            (value: UserTree) => {
-                dispatch({
-                    type: userTreeActionTypes.UPDATE_USER_TREE_SUCCEEDED,
-                    payload: value,
-                });
-            },
-            (error: any) => {
-                dispatch({
-                    type: userTreeActionTypes.UPDATE_USER_TREE_FAILED,
-                });
-            }
-        )
-    };
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.UPDATE_USER_TREE_REQUESTED,
+		});
+		apiClient.updateUserTree(record).then(
+			(value: UserTree) => {
+				dispatch({
+					type: userTreeActionTypes.UPDATE_USER_TREE_SUCCEEDED,
+					payload: value,
+				});
+			},
+			(error: any) => {
+				dispatch({
+					type: userTreeActionTypes.UPDATE_USER_TREE_FAILED,
+				});
+			},
+		);
+	};
 };
-
 
 export const unassignUserTrees = (saplingIds: string[]) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.UNASSIGN_USER_TREES_REQUESTED,
-        });
-        apiClient.unassignUserTrees(saplingIds).then(
-            () => {
-                toast.success("Unassigned trees successfully!");
-                dispatch({
-                    type: userTreeActionTypes.UNASSIGN_USER_TREES_SUCCEEDED,
-                });
-            },
-            (error: any) => {
-                console.error(error);
-                toast.error("Failed to unassign trees!");
-                dispatch({
-                    type: userTreeActionTypes.UNASSIGN_USER_TREES_FAILED,
-                });
-            }
-        )
-    };
-};
-
-
-export const getUserTreeCount = (offset: number, limit: number, filters?: any) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.GET_USER_TREE_COUNT_REQUESTED,
-        });
-        apiClient.getUserTreeCount(offset, limit, filters).then(
-            (result: UserTreeCountPaginationResponse) => {
-                dispatch({
-                    type: userTreeActionTypes.GET_USER_TREE_COUNT_SUCCEEDED,
-                    payload: result,
-                });
-            },
-            (error: any) => {
-                console.error(error);
-                dispatch({
-                    type: userTreeActionTypes.GET_USER_TREE_COUNT_FAILED,
-                    value: error
-                });
-            }
-        )
-    };
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.UNASSIGN_USER_TREES_REQUESTED,
+		});
+		apiClient.unassignUserTrees(saplingIds).then(
+			() => {
+				toast.success('Unassigned trees successfully!');
+				dispatch({
+					type: userTreeActionTypes.UNASSIGN_USER_TREES_SUCCEEDED,
+				});
+			},
+			(error: any) => {
+				console.error(error);
+				toast.error('Failed to unassign trees!');
+				dispatch({
+					type: userTreeActionTypes.UNASSIGN_USER_TREES_FAILED,
+				});
+			},
+		);
+	};
 };
 
 export const unMapTrees = (saplingIds: string[]) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.UN_MAP_TREES_REQUESTED,
-        });
-        apiClient.removeTreeMappings(saplingIds).then(
-            () => {
-                toast.success("Unmapped trees successfully!");
-                dispatch({
-                    type: userTreeActionTypes.UN_MAP_TREES_SUCCEEDED,
-                });
-            },
-            (error: any) => {
-                toast.error(error.message);
-                dispatch({
-                    type: userTreeActionTypes.UN_MAP_TREES_FAILED,
-                    value: error
-                });
-            }
-        )
-    };
-}
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.UN_MAP_TREES_REQUESTED,
+		});
+		apiClient.removeTreeMappings(saplingIds).then(
+			() => {
+				toast.success('Unmapped trees successfully!');
+				dispatch({
+					type: userTreeActionTypes.UN_MAP_TREES_SUCCEEDED,
+				});
+			},
+			(error: any) => {
+				toast.error(error.message);
+				dispatch({
+					type: userTreeActionTypes.UN_MAP_TREES_FAILED,
+					value: error,
+				});
+			},
+		);
+	};
+};
 
 export const mapTrees = (request: MapTreesUsingSaplingIdsRequest) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.MAP_USER_TREES_REQUESTED,
-        });
-        apiClient.mapTrees(request).then(
-            () => {
-                toast.success("Mapped trees successfully!");
-                dispatch({
-                    type: userTreeActionTypes.MAP_USER_TREES_SUCCEEDED,
-                });
-            },
-            (error: any) => {
-                console.error(error);
-                toast.error("Failed to map trees!");
-                dispatch({
-                    type: userTreeActionTypes.MAP_USER_TREES_FAILED,
-                    value: error
-                });
-            }
-        )
-    };
-}
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.MAP_USER_TREES_REQUESTED,
+		});
+		apiClient.mapTrees(request).then(
+			() => {
+				toast.success('Mapped trees successfully!');
+				dispatch({
+					type: userTreeActionTypes.MAP_USER_TREES_SUCCEEDED,
+				});
+			},
+			(error: any) => {
+				console.error(error);
+				toast.error('Failed to map trees!');
+				dispatch({
+					type: userTreeActionTypes.MAP_USER_TREES_FAILED,
+					value: error,
+				});
+			},
+		);
+	};
+};
 
 export const mapTreesForPlot = (request: MapTreesUsingPlotIdRequest) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.MAP_USER_TREES_IN_PLOT_REQUESTED,
-        });
-        apiClient.mapTreesForPlot(request).then(
-            () => {
-                toast.success("Mapped trees successfully!");
-                dispatch({
-                    type: userTreeActionTypes.MAP_USER_TREES_IN_PLOT_SUCCEEDED,
-                });
-            },
-            (error: any) => {
-                console.error(error);
-                toast.error("Failed to map trees!");
-                dispatch({
-                    type: userTreeActionTypes.MAP_USER_TREES_IN_PLOT_FAILED,
-                    value: error
-                });
-            }
-        )
-    };
-}
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.MAP_USER_TREES_IN_PLOT_REQUESTED,
+		});
+		apiClient.mapTreesForPlot(request).then(
+			() => {
+				toast.success('Mapped trees successfully!');
+				dispatch({
+					type: userTreeActionTypes.MAP_USER_TREES_IN_PLOT_SUCCEEDED,
+				});
+			},
+			(error: any) => {
+				console.error(error);
+				toast.error('Failed to map trees!');
+				dispatch({
+					type: userTreeActionTypes.MAP_USER_TREES_IN_PLOT_FAILED,
+					value: error,
+				});
+			},
+		);
+	};
+};
 
 export const assignTrees = (data: FormData) => {
-    const apiClient = new ApiClient();
-    return (dispatch: any) => {
-        dispatch({
-            type: userTreeActionTypes.ASSIGN_USER_TREES_REQUESTED,
-        });
-        apiClient.assignUserTrees(data).then(
-            () => {
-                toast.success("Assigned trees successfully!");
-                dispatch({
-                    type: userTreeActionTypes.ASSIGN_USER_TREES_SUCCEEDED,
-                });
-            },
-            (error: any) => {
-                console.error(error);
-                toast.error("Failed to assign trees!");
-                dispatch({
-                    type: userTreeActionTypes.ASSIGN_USER_TREES_FAILED,
-                    value: error
-                });
-            }
-        )
-    };
-}
+	const apiClient = new ApiClient();
+	return (dispatch: any) => {
+		dispatch({
+			type: userTreeActionTypes.ASSIGN_USER_TREES_REQUESTED,
+		});
+		apiClient.assignUserTrees(data).then(
+			() => {
+				toast.success('Assigned trees successfully!');
+				dispatch({
+					type: userTreeActionTypes.ASSIGN_USER_TREES_SUCCEEDED,
+				});
+			},
+			(error: any) => {
+				console.error(error);
+				toast.error('Failed to assign trees!');
+				dispatch({
+					type: userTreeActionTypes.ASSIGN_USER_TREES_FAILED,
+					value: error,
+				});
+			},
+		);
+	};
+};
--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,51 +1,56 @@
-import { combineReducers } from "redux";
-import { plantTypesDataReducer, searchPlantTypesDataReducer } from "./plantTypeReducer";
-import { onsiteStaffsDataReducer } from "./onsiteStaffReducer";
-import { groupsDataReducer, searchGroupsDataReducer } from "./groupReducer";
-import { plotsDataReducer, searchPlotsDataReducer } from "./plotReducer";
-import { sitesDataReducer } from "./siteReducer";
-import { pondsDataReducer, searchPondsDataReducer } from "./pondReducer";
-import { pondWaterLevelUpdatesDataReducer } from "./pondWaterLevelUpdateReducer";
-import { treesDataReducer } from "./treeReducer";
-import { usersDataReducer, searchUsersDataReducer } from "./userReducer";
-import { userTreesDataReducer, userTreeCountDataReducer } from "./userTreeReducer";
-import { userGroupsDataReducer } from "./userGroupReducer";
-import { donationsDataReducer } from "./donationReducer";
-import { eventsDataReducer } from "./eventReducer";
-import { visitsDataReducer } from "./visitReducer";
-import { visitUsersDataReducer} from "./visitUserReducer";
-import { treeImagesDataReducer } from "./treeImageReducer";
-import { giftCardUsersDataReducer, giftCardsDataReducer } from "./giftCardReducer";
-import { tagsDataReducer } from "./tagReducer";
-import { campaignsDataReducer } from "./campaignReducer";
+import { combineReducers } from 'redux';
+import {
+	plantTypesDataReducer,
+	searchPlantTypesDataReducer,
+} from './plantTypeReducer';
+import { onsiteStaffsDataReducer } from './onsiteStaffReducer';
+import { groupsDataReducer, searchGroupsDataReducer } from './groupReducer';
+import { plotsDataReducer, searchPlotsDataReducer } from './plotReducer';
+import { sitesDataReducer } from './siteReducer';
+import { pondsDataReducer, searchPondsDataReducer } from './pondReducer';
+import { pondWaterLevelUpdatesDataReducer } from './pondWaterLevelUpdateReducer';
+import { treesDataReducer } from './treeReducer';
+import { usersDataReducer, searchUsersDataReducer } from './userReducer';
+import { userTreesDataReducer } from './userTreeReducer';
+import { userGroupsDataReducer } from './userGroupReducer';
+import { donationsDataReducer } from './donationReducer';
+import { eventsDataReducer } from './eventReducer';
+import { visitsDataReducer } from './visitReducer';
+import { visitUsersDataReducer } from './visitUserReducer';
+import { treeImagesDataReducer } from './treeImageReducer';
+import {
+	giftCardUsersDataReducer,
+	giftCardsDataReducer,
+} from './giftCardReducer';
+import { tagsDataReducer } from './tagReducer';
+import { campaignsDataReducer } from './campaignReducer';
 
 const rootReducer = combineReducers({
-    plantTypesData: plantTypesDataReducer,
-    onsiteStaffsData: onsiteStaffsDataReducer,
-    groupsData: groupsDataReducer,
-    userGroupsData: userGroupsDataReducer,
-    sitesData: sitesDataReducer,
-    plotsData: plotsDataReducer,
-    pondsData: pondsDataReducer,
-    pondWaterLevelUpdatesData: pondWaterLevelUpdatesDataReducer,
-    treesData: treesDataReducer,
-    treeImagesData: treeImagesDataReducer,
-    usersData: usersDataReducer,
-    userTreesData: userTreesDataReducer,
-    userTreeCountData: userTreeCountDataReducer,
-    searchPlantTypesData: searchPlantTypesDataReducer,
-    searchPondsData: searchPondsDataReducer,
-    searchPlotsData: searchPlotsDataReducer,
-    searchUsersData: searchUsersDataReducer,
-    searchGroupsData: searchGroupsDataReducer,
-    donationsData: donationsDataReducer,
-    eventsData: eventsDataReducer,
-    visitsData:visitsDataReducer,
-    visitUserData: visitUsersDataReducer,
-    giftCardsData: giftCardsDataReducer,
-    giftCardUsersData: giftCardUsersDataReducer,
-    tagsData: tagsDataReducer,
-    campaignsData: campaignsDataReducer,
+	plantTypesData: plantTypesDataReducer,
+	onsiteStaffsData: onsiteStaffsDataReducer,
+	groupsData: groupsDataReducer,
+	userGroupsData: userGroupsDataReducer,
+	sitesData: sitesDataReducer,
+	plotsData: plotsDataReducer,
+	pondsData: pondsDataReducer,
+	pondWaterLevelUpdatesData: pondWaterLevelUpdatesDataReducer,
+	treesData: treesDataReducer,
+	treeImagesData: treeImagesDataReducer,
+	usersData: usersDataReducer,
+	userTreesData: userTreesDataReducer,
+	searchPlantTypesData: searchPlantTypesDataReducer,
+	searchPondsData: searchPondsDataReducer,
+	searchPlotsData: searchPlotsDataReducer,
+	searchUsersData: searchUsersDataReducer,
+	searchGroupsData: searchGroupsDataReducer,
+	donationsData: donationsDataReducer,
+	eventsData: eventsDataReducer,
+	visitsData: visitsDataReducer,
+	visitUserData: visitUsersDataReducer,
+	giftCardsData: giftCardsDataReducer,
+	giftCardUsersData: giftCardUsersDataReducer,
+	tagsData: tagsDataReducer,
+	campaignsData: campaignsDataReducer,
 });
 
 export default rootReducer;
